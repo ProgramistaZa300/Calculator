c14a8940906f390d0237919099645adbeeb6b2c2
Maciej Klimiuk <maciek120200@gmail.com>
2023-10-10 13:48:14 +0200

    swift

---
+++ Calculator/ButtonView.swift
@@ -0,0 +1,8 @@
+//
+//  ButtonView.swift
+//  Calculator
+//
+//  Created by student on 10/10/2023.
+//
+
+import Foundation
---
+++ Calculator/Operations.swift
@@ -0,0 +1,8 @@
+//
+//  Operations.swift
+//  Calculator
+//
+//  Created by student on 10/10/2023.
+//
+
+import Foundation
